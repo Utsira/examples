bbac8ed651ba39838e2e5426d17218d31750f160
Oliver-D <olidew@MacBook-Air.lan>
2015-05-27 17:18:35 +0100

    Changed shader look-up method for faster framerate

--- 3D Animation Loader v2.lua
+++ 3D Animation Loader v2.lua
@@ -703,8 +703,7 @@ TilerShader={vs=[[
 --Shaders
 FrameBlendNoTex = { --models with no texture image
     splineVert= --vertex shader with catmull rom spline interpolation of key frames
-    [[ 
-
+    [[   
     uniform mat4 modelViewProjection;
     uniform mat4 modelMatrix;
     uniform float ambient; // --strength of ambient light 0-1
@@ -724,17 +723,31 @@ FrameBlendNoTex = { --models with no texture image
     attribute vec3 position2; //not possible for attributes to be arrays in Gl Es2.0 
     attribute vec3 position3;
     attribute vec3 position4;
-    attribute vec3 position5;
-    attribute vec3 position6;
-        
+    
     attribute vec3 normal;
     attribute vec3 normal1;
     attribute vec3 normal2;
     attribute vec3 normal3;
     attribute vec3 normal4;
-    attribute vec3 normal5;
-    attribute vec3 normal6;
-      
+    
+    vec3 getPos(int no) //home-made hash, ho hum.  
+    {
+        if (no==0) return position;
+        if (no==1) return position1;
+        if (no==2) return position2;
+        if (no==3) return position3;
+        if (no==4) return position4;
+    }
+    
+    vec3 getNorm(int no)
+    {
+        if (no==0) return normal;
+        if (no==1) return normal1;
+        if (no==2) return normal2;
+        if (no==3) return normal3;
+        if (no==4) return normal4;
+    }
+          
     varying lowp vec4 vAmbient;
     varying lowp vec4 vColor;
     varying vec4 vDirectDiffuse;
@@ -749,26 +762,8 @@ FrameBlendNoTex = { --models with no texture image
     
     void main()
     {       
-        vec3 pos[7];
-        pos[0] = position;
-        pos[1] = position1;
-        pos[2] = position2;
-        pos[3] = position3;
-        pos[4] = position4;
-        pos[5] = position5;
-        pos[6] = position6;
-   
-        vec3 nor[7];
-        nor[0] = normal;
-        nor[1] = normal1;
-        nor[2] = normal2;
-        nor[3] = normal3;
-        nor[4] = normal4;
-        nor[5] = normal5;
-        nor[6] = normal6;
- 
-     vec3 framePos = CatmullRom(frameBlend, frameBlend2, frameBlend3, pos[frames[0] ], pos[frames[1] ], pos[frames[2] ], pos[frames[3] ] ); // nb must have space between 2 ] ]
-       vec3 frameNorm = CatmullRom(frameBlend, frameBlend2, frameBlend3, nor[frames[0] ], nor[frames[1] ], nor[frames[2] ], nor[frames[3] ] );
+        vec3 framePos = CatmullRom(frameBlend, frameBlend2, frameBlend3, getPos(frames[0]), getPos(frames[1]), getPos(frames[2]), getPos(frames[3]) );
+       vec3 frameNorm = CatmullRom(frameBlend, frameBlend2, frameBlend3, getNorm(frames[0]), getNorm(frames[1]), getNorm(frames[2]), getNorm(frames[3]) );
     
         vec4 norm = normalize(modelMatrix * vec4( frameNorm, 0.0 ));
 
@@ -798,20 +793,30 @@ FrameBlendNoTex = { --models with no texture image
     attribute vec4 color;
 
     attribute vec3 position;
-    attribute vec3 position1;
     attribute vec3 position2; //not possible for attributes to be arrays in Gl Es2.0 
     attribute vec3 position3;
     attribute vec3 position4;
-    attribute vec3 position5;
-    attribute vec3 position6;
-        
+    
     attribute vec3 normal;
-    attribute vec3 normal1;
     attribute vec3 normal2;
     attribute vec3 normal3;
     attribute vec3 normal4;
-    attribute vec3 normal5;
-    attribute vec3 normal6;
+    
+    vec3 getPos(int no) //home-made hash, ho hum.  
+    {
+        if (no==1) return position;
+        if (no==2) return position2;
+        if (no==3) return position3;
+        if (no==4) return position4;
+    }
+    
+    vec3 getNorm(int no)
+    {
+        if (no==1) return normal;
+        if (no==2) return normal2;
+        if (no==3) return normal3;
+        if (no==4) return normal4;
+    }
     
     varying lowp vec4 vAmbient;
     varying lowp vec4 vColor;
@@ -819,26 +824,8 @@ FrameBlendNoTex = { --models with no texture image
     
     void main()
     {
-        vec3 pos[7];
-        pos[0] = position;
-        pos[1] = position1;
-        pos[2] = position2;
-        pos[3] = position3;
-        pos[4] = position4;
-        pos[5] = position5;
-        pos[6] = position6;
-   
-        vec3 nor[7];
-        nor[0] = normal;
-        nor[1] = normal1;
-        nor[2] = normal2;
-        nor[3] = normal3;
-        nor[4] = normal4;
-        nor[5] = normal5;
-        nor[6] = normal6;
-
-        vec3 framePos = mix(pos[frames[2] ], pos[frames[3] ], frameBlend);
-        vec3 frameNorm = mix(nor[frames[2] ], nor[frames[3] ], frameBlend);
+        vec3 framePos = mix(getPos(frames[2]), getPos(frames[3]), frameBlend);
+        vec3 frameNorm = mix(getNorm(frames[2]), getNorm(frames[3]), frameBlend);
        
         vec4 norm = normalize(modelMatrix * vec4( frameNorm, 0.0 ));
         vDirectDiffuse = lightColor * max( 0.0, dot( norm, light )); // direct color    
@@ -1026,3 +1013,4 @@ FrameBlendNoTex = { --models with no texture image
     
     ]]
 }
+
